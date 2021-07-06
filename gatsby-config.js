4701faad355ef87a0c7b505d0bb36044bc6cf4a0
masakitm <mtz.180@gmail.com>
2021-07-06 16:32:52 +0900

    add: redux

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-mdx",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -13,6 +16,23 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: `gatsby-plugin-react-redux`,
+      options: {
+        // [required] - path to your createStore module
+        pathToCreateStoreModule: './src/store/createStore',
+        serialize: {
+          space: 0,
+          isJSON: true,
+          unsafe: false,
+          ignoreFunction: true,
+        },
+        // [optional] - if true will clean up after itself on the client, default:
+        cleanupOnClient: true,
+        // [optional] - name of key on `window` where serialized state will be stored, default:
+        windowKey: '__PRELOADED_STATE__',
+      },
     }
   ]
 };
